6cab39770b85af4ff1be8b090dd02f520fbcf48a
CCB Admin <admin@ccb.edu.ph>
2026-04-20 16:19:10 +0800

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -2893,25 +2893,3 @@ app.listen(PORT, () => {
   logger.info(`Health check available at: http://localhost:${PORT}/health`);
 });
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-app.get("/test-db", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.json({
-      success: true,
-      time: result.rows,
-    });
-  } catch (err) {
-    console.error("DB ERROR:", err);
-    res.status(500).json({
-      success: false,
-      error: err.message,
-    });
-  }
-});
